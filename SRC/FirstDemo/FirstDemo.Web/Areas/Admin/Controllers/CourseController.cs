e449495355b7be226a8d0349a7648dc5a3807ffe
HasanurRashid <hasan6221@gmail.com>
2024-06-30 23:42:44 +0600

    * jQuery  data table (static) integration * A sample form design

---
+++ SRC/FirstDemo/FirstDemo.Web/Areas/Admin/Controllers/CourseController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace FirstDemo.Web.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class CourseController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Create()
+        {
+            return View();
+        }
+    }
+}
