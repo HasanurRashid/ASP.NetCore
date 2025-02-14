b9bd2f178ddd4636f737f3ccc6deee861278f854
Hasan <hasan6221@gmail.com>
2025-02-14 23:16:23 +0600

    Data table & form integration

---
+++ FisrtDemo/FirstDemo.Web/Areas/Admin/Controllers/CourseController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace FirstDemo.Web.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class CourseController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+    }
+}
